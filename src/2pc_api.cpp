f6c7f72842119676dbb9bf1e366b95ca3a0ec933
xingpz2008 <p.xing@std.uestc.edu.cn>
2023-12-08 06:51:24 +0000

    Add offset gate for FSS Gen (Correctness unverified)

---
+++ src/2pc_api.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 12/6/23.
+//
+
+#include "2pc_api.h"
---
+++ src/2pc_api.h
@@ -0,0 +1,8 @@
+//
+// Created by root on 12/6/23.
+//
+
+#ifndef EZPC_2PC_API_H
+#define EZPC_2PC_API_H
+
+#endif //EZPC_2PC_API_H
